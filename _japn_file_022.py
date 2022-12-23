f98e3495abe05cba17496517d6b7bf1e96f0e359
Rajkumarks201 <117342543+Rajkumarks201@users.noreply.github.com>
2022-12-23 18:21:04 +0530

    states and capital.py
    states and cpital.py

---
+++ _japn_file_022.py
@@ -0,0 +1,17 @@
+{
+    "State":"Telangana",
+    "capital":"Hyderabad",
+    "state":"jammu and kashmir",
+    "capital":"srinager",
+    "state":"tamil nadu",
+    "capital":"chennai",
+    "state":"Rajashtan",
+    "capital":"jaipur",
+    "state":"goa",
+    "capital":"panaji",
+    "state":"west bengal",
+    "capital":"kolkata",
+    "state":"jarkand",
+    "capital":"ranchi"}
+
+
